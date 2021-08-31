06c4c3b3e288f371292d2e8ab1050c896e20deed
cxplanet <cxplanet@hotmail.com>
2021-08-31 10:09:25 -0700

    Update constants file

--- CryptoApp/Constants.swift
+++ CryptoApp/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Api {
-    private static let apiToken = "c4js56qad3idfmhpas6g"
+    private static let apiToken = "<FINNHUBB_TOKEN>"
     static let SYMBOL_LIST_ENDPOINT = "https://finnhub.io/api/v1/crypto/symbol?exchange=binance&token=\(apiToken)"
     static let SYMBOL_REALTIME_ENDPOINT = "wss://ws.finnhub.io?token=\(apiToken)"
 }
